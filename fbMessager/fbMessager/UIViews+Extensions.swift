18f1c0d47f30b91c029f1efff563752d779f1d15
MAC <mac@Air-cua-MAC.net.fpt>
2018-03-18 07:19:56 +0700

    update code

--- fbMessager/fbMessager/UIViews+Extensions.swift
+++ fbMessager/fbMessager/UIViews+Extensions.swift
@@ -10,7 +10,6 @@ import UIKit
 
 extension UIView {
     
-    
     func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
         
--- fbMessager/fbMessager/ViewController.swift
+++ fbMessager/fbMessager/ViewController.swift
@@ -12,19 +12,20 @@ class ViewController: UIViewController {
 
     private let cellId = "Cell"
     
-    private let collectionView: UICollectionView = {
+    lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
+        collectionView.alwaysBounceVertical = true
+        collectionView.dataSource = self
+        collectionView.delegate = self
         return collectionView
     }()
-    
+    // ep1: 8:30
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(collectionView)
         collectionView.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
-        collectionView.dataSource = self
-        collectionView.delegate = self
         collectionView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .zero, size: .zero)
     }
 }
@@ -39,19 +40,31 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
 }
 
-class FriendCell: UICollectionViewCell {
+class FriendCell: BaseCell {
+    
+    override func setupViews() {
+        backgroundColor = .yellow
+    }
+}
+
+class BaseCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
+        setupViews()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    func setupViews() {
+    }
 }
 
