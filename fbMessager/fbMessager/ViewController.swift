19e91ca0fdd3599a3649b344656ecb188beea5e4
MAC <mac@Air-cua-MAC.net.fpt>
2018-03-21 23:15:15 +0700

    create view

--- fbMessager/fbMessager/ViewController.swift
+++ fbMessager/fbMessager/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
     // ep1: 8:30
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Recent"
+        
         view.addSubview(collectionView)
         collectionView.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
         collectionView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .zero, size: .zero)
@@ -48,8 +50,86 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, 
 
 class FriendCell: BaseCell {
     
+    let profileImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "zurkerberg")
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 34
+        iv.layer.masksToBounds = true
+        return iv
+    }()
+    
+    let dividerLineView: UIView = {
+        let v = UIView()
+        v.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
+        return v
+    }()
+    
+    let containView : UIView = {
+        let containView = UIView()
+        return containView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Friend Name"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your friend's message and something else..."
+        label.textColor = .darkGray
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12: 05 pm"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textAlignment = .right
+        return label
+    }()
+    
+    let hasRedImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "zurkerberg")
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 10
+        iv.layer.masksToBounds = true
+        return iv
+    }()
+    
     override func setupViews() {
-        backgroundColor = .yellow
+        addSubview(profileImage)
+        addSubview(dividerLineView)
+        setupContainerView()
+        
+        profileImage.anchor(top: nil, leading: centerXAnchor, bottom: nil, trailing: nil, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 68, height: 68))
+        profileImage.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        profileImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5).isActive = true
+        
+        dividerLineView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 82, bottom: 0, right: 0), size: .init(width: 0, height: 1))
+    }
+    
+    private func setupContainerView() {
+        addSubview(containView)
+        containView.addSubview(nameLabel)
+        containView.addSubview(messageLabel)
+        containView.addSubview(timeLabel)
+        containView.addSubview(hasRedImageView)
+        
+        containView.anchor(top: profileImage.topAnchor, leading: profileImage.trailingAnchor, bottom: profileImage.bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 5, bottom: 0, right: 0), size: .init(width: 0, height: 0))
+        
+        nameLabel.anchor(top: containView.topAnchor, leading: containView.leadingAnchor, bottom: nil, trailing: timeLabel.leadingAnchor, padding: .init(top: 4, left: 4, bottom: 0, right: 0), size: .init(width: 0, height: 20))
+        
+        messageLabel.anchor(top: nameLabel.bottomAnchor, leading: containView.leadingAnchor, bottom: containView.bottomAnchor, trailing: containView.trailingAnchor, padding: .init(top: 0, left: 4, bottom: 0, right: 0), size: .init(width: 0, height: 20))
+        
+        timeLabel.anchor(top: containView.topAnchor, leading: nil, bottom: nil, trailing: containView.trailingAnchor, padding: .init(top: 4, left: 0, bottom: 0, right: 4), size: .init(width: 100, height: 20))
+        
+        hasRedImageView.anchor(top: timeLabel.bottomAnchor, leading: nil, bottom: nil, trailing: containView.trailingAnchor, padding: .init(top: 4, left: 0, bottom: 0, right: 4), size: .init(width: 20, height: 20))
     }
 }
 
