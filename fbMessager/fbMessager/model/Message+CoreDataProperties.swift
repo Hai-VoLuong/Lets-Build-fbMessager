3fcd70bd72e0c1ccadd80b713b7894ecc4c7a503
MAC <mac@Air-cua-MAC.net.fpt>
2018-03-28 22:14:49 +0700

    create chat bubble right

--- fbMessager/fbMessager/model/Message+CoreDataProperties.swift
+++ fbMessager/fbMessager/model/Message+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Message+CoreDataProperties.swift
 //  fbMessager
 //
-//  Created by MAC on 3/25/18.
+//  Created by MAC on 3/28/18.
 //  Copyright © 2018 MAC. All rights reserved.
 //
 //
@@ -17,8 +17,9 @@ extension Message {
         return NSFetchRequest<Message>(entityName: "Message")
     }
 
-    @NSManaged public var text: String?
     @NSManaged public var date: NSDate?
+    @NSManaged public var text: String?
+    @NSManaged public var isSender: Bool
     @NSManaged public var friend: Friend?
 
 }
